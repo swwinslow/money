dbb7b0fbace1a0b902de228e0f8c716967731622
Seth Winslow <swwinlow@bsu.eudu>
2017-01-05 14:53:24 -0500

    Started to update the front end with Complete View Controller

--- frontend/controllers/HomePageController.js
+++ frontend/controllers/HomePageController.js
@@ -1,18 +1,13 @@
-app.controller('HomePageController', function(CONFIG, $scope, $location){
+app.controller('HomePageController', function(CONFIG, $scope, $location, CompleteFactory){
+    
+    $scope.totalIRA = 0;
+    $scope.totalTrans = 0;
+    $scope.totalPay = 0;
+    
+    CompleteFactory.getIRATotal().then(function (response){
+        $scope.totalIRA = response.data.data.amount;
+    });
 
-    $scope.investment = function(){
-        $location.path("/investments");
-    };
-    
-    $scope.pay = function(){
-        $location.path("/pay");
-    };
-    
-    $scope.transaction = function(){
-        $location.path("/transaction");
-    };
-    
-    $scope.home = function(){
-        $location.path("/");
-    }
+
+
 });
---
+++ frontend/factories/completeFactory.js
@@ -0,0 +1,20 @@
+app.factory('CompleteFactory', function($http, $rootScope) {
+
+    var data = {};
+    var baseUrl = location.origin +'/money/backend/public/api/complete';
+
+    
+    data.getIRATotal = function() {
+        return $http.get(baseUrl + "/IRATotal");
+    }
+    
+    data.getTransTotal = function() {
+        return $http.get(baseUrl + "/TransTotal");
+    }
+    
+    data.getPayTotal = function() {
+        return $http.get(baseUrl + "/PayTotal");
+    }
+
+    return data;
+});
